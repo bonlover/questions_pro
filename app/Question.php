63d11de6b3603bea2cb9bca764d7217dd6598e01
vagrant <vagrant@homestead>
2019-03-24 06:16:14 +0000

    Add author info and question creation date in questiin index page

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $filliable = ['title','body','views','answers','votes'];
+    protected $filliable = ['title','body'];
     
     /**
      * The relationship between User model is here.
@@ -27,5 +27,15 @@ class Question extends Model
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute() {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans();
+        // return $this->created_at->format("d/m/y");
+
     }
 }
