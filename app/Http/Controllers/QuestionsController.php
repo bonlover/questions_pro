538e789f3dcaba84c544bb77006385ced01b5c2b
vagrant <vagrant@homestead>
2019-03-26 06:15:37 +0000

    Implement authorization using policies and protected questions using middleware

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -9,6 +9,15 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    /** 
+     * Middleware construct to protect from edit and delete.
+     * Display index and show pages using except.
+    */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -64,6 +73,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update", $question);
+
         return view('questions.edit', compact('question'));
     }
 
@@ -76,6 +87,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update", $question);
+
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', "Your question has been updated.");
     }
@@ -88,6 +101,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize("delete", $question);
+
         $question->delete();
         return redirect('/questions')->with('success', "Your question has been deleted.");
     }
