5ade92ee116ee2985b39c5d85c55557b0f3c1d0f
vagrant <vagrant@homestead>
2019-03-27 11:51:22 +0000

    Add votes control and Answer check with fontAwesome packages

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,9 @@
+
+import {library, dom} from '@fortawesome/fontawesome-svg-core';
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import {faCaretUp} from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import {faStar} from '@fortawesome/free-solid-svg-icons/faStar';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+
+library.add([faCaretDown, faCaretUp, faCheck, faStar]);
+dom.watch();
